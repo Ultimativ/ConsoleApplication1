a71aa801bf5fe2346c7ba2af357e0f92705d776f
Me <Me@ME>
2017-03-27 12:59:39 +0200

    while

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,28 +11,32 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string userName;
-            string userEmail;
-            int userAge;   
-                                       
+            while (true)
+            {
+
+
+                string userName;
+                string userEmail;
+                int userAge;
+
                 Console.Write("Geben sie ihren Namen:");
                 userName = Console.ReadLine();
                 Console.WriteLine("Hallo " + userName);
 
-                    if (Regex.IsMatch(userName, @"^[a-zA-Z]+$"))
-                    {
-                        Console.WriteLine("Richtig");
-                    }
+                if (Regex.IsMatch(userName, @"^[a-zA-Z]+$"))
+                {
+                    Console.WriteLine("Richtig");
+                }
 
-                    else
+                else
 
-                    {
-                        Console.WriteLine("Falsch");
-                    }
+                {
+                    Console.WriteLine("Falsch");
+                }
 
                 Console.Write("Bitte geben sie ihr alter ein: ");
                 userAge = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Sie sind " + userAge +  " Jahre alt ");
+                Console.WriteLine("Sie sind " + userAge + " Jahre alt ");
                 Console.WriteLine("Bitte geben sie hier ihre E-mail adresse ein: ");
                 userEmail = Console.ReadLine();
                 Console.WriteLine("Ihre e-mail adresse lautet: " + userEmail);
@@ -41,8 +45,8 @@ namespace ConsoleApplication1
                 Console.ReadLine();
 
 
-              
-            
+
+            }
         }
     }
 
